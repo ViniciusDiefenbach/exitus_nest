d3962d6a6426f2b9b561bfbb614d74d961dbe49b
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-05-11 23:20:32 -0300

    fix: .env PORT + app.module correction

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { LogsModule } from './logs/logs.module';
 import { LogModule } from './log/log.module';
 import { EarlyExitModule } from './early-exit/early-exit.module';
 
 @Module({
-  imports: [UserModule, LogsModule, LogModule, EarlyExitModule],
+  imports: [UserModule, LogModule, EarlyExitModule],
   controllers: [AppController],
   providers: [AppService],
 })
